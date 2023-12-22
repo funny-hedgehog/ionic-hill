c630a5b11352100038dbd6bf60116e420051859f
arifm <arifmho@yandex.ru>
2023-12-22 10:44:40 +0300

    'ionic-3'

--- src/pages/OverLoadedPage.js
+++ src/pages/OverLoadedPage.js
@@ -2,19 +2,27 @@ import {IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, Io
 import React from 'react';
 
 const urls1 = [
-  'http://i1.wallbox.ru/wallpapers/main2/201719/gorod-doma-sumerki.jpg',
-  'https://www.zastavki.com/pictures/originals/2021Nature___Mountains_Tall_green_fir_trees_are_reflected_in_the_lake_against_the_background_of_mountains_154658_.jpg',
-  'https://i.pinimg.com/originals/0d/0c/da/0d0cda50d82a825d602ad45547112b0d.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/4/42/Plain_Landscape.jpg',
 ];
 
 const urls2 = [
-  'https://sneg.top/uploads/posts/2023-03/1678572700_sneg-top-p-sbornik-oboev-dlya-rabochego-stola-priroda-1.jpg',
-  'https://img.goodfon.ru/original/2880x1800/e/21/maroon-lake-ozero-gory-derevya.jpg',
-  'https://w.forfun.com/fetch/f6/f6344ac128b14d56675be186a789d8d1.jpeg',
+  'https://wallbox.ru/wallpapers/main2/201717/nebo-dereva-ozero-gory-les-vodoem.jpg',
 ]
 
 function OverLoadedPage() {
-  const [url, setUrl] = React.useState(['first', 'first', 'first'])
+  const [url, setUrl] = React.useState(['first'])
+  const [photosSize, setPhotosSize] = React.useState(0)
+  const [arraySize, setArraySize] = React.useState(0)
+
+  React.useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then(json => setArraySize(s => s += 135))
+      .catch((e) => console.log(`Error ${e}`))
+  }, [arraySize]);
 
   return (
     <IonPage>
@@ -35,15 +43,19 @@ function OverLoadedPage() {
       <IonContent>
         <IonToolbar>
           <IonTitle>OverLoadedPage</IonTitle>
-          <ul>
+          <h2>Объём загруженного массива - {Math.round(arraySize / 1000)} MB</h2>
+          <h2>Объём загруженных фотографий  - {Math.round(photosSize)} MB</h2>
+          <ul style={{listStyle: 'none'}}>
             {url.map((item, index) => {
               return (
                 <li key={index}>
                   <img
+                    style={{opacity: 0}}
                     onLoad={() => {
                       const arr = url
                       arr[index] = item === 'first' ? 'second' : 'first'
                       setUrl([...arr])
+                      setPhotosSize(s => s += 1.1)
                     }}
                     alt={`${index}`}
                     src={item === 'first' ? urls1[index] : urls2[index]}
